3a616449e7da95454f6ab262437cac87a3a08bf3
EBK21 <eebssk1@ebktech21.xyz>
2018-10-14 22:47:14 +0800

    Bulk patch to 4.4.161.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -347,7 +347,8 @@ static struct ctl_table kern_table[] = {
 		.data		= &sysctl_sched_cstate_aware,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &one,
 	},
 	{
 		.procname	= "sched_wakeup_granularity_ns",
